625b8ef03a30504ef72defe87327c3b20b906893
Leonards03 <edwin.leonards03@gmail.com>
2021-06-22 16:46:03 +0700

    Update AppPreferences.kt

--- app/src/main/java/com/dicoding/tmdb/app/utils/AppPreferences.kt
+++ app/src/main/java/com/dicoding/tmdb/app/utils/AppPreferences.kt
@@ -1,14 +1,16 @@
 package com.dicoding.tmdb.app.utils
 
 import android.content.Context
+import androidx.core.content.edit
 import androidx.preference.PreferenceManager
 import com.dicoding.tmdb.core.utils.ImageSize
 
 class AppPreferences(context: Context) {
     private val preferences = PreferenceManager.getDefaultSharedPreferences(context)
     fun getImageSize(): ImageSize {
-        val imageSize = preferences.getString(image_size_key, String())
+        val imageSize = preferences.getString(image_size_key, null)
         return if (imageSize == null) {
+            preferences.edit(commit = true) { putString(image_size_key, ImageSize.Medium.name) }
             ImageSize.Medium
         } else {
             ImageSize.valueOf(imageSize)
